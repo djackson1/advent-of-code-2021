f1511d78ad19a827cfac336c19b795365093cfec
Daniel Jackson <d95jackson@gmail.com>
2021-12-18 19:20:53 +0000

    part b test

--- src/day07/main.js
+++ src/day07/main.js
@@ -7,14 +7,23 @@ function getPuzzleInput(isTestInput = false) {
 }
 const input = getPuzzleInput();
 
-function findShortestAmountOfFuel(inputs) {
+function triangleNumbersFn(a, b) {
+  const distance = Math.abs(a - b);
+  return (distance * (distance + 1)) / 2;
+}
+
+function linearFn(a, b) {
+  return Math.abs(a - b);
+}
+
+function findShortestAmountOfFuel(inputs, distanceFunction) {
   const max = inputs.reduce((acc, v) => (v > acc ? v : acc), 0);
 
   const values = [...new Array(max + 1)].map((_, i) => i);
 
   const fuelCounts = values.map((position) => {
     return inputs.reduce((acc, crabPosition) => {
-      const distance = Math.abs(position - crabPosition);
+      const distance = distanceFunction(position, crabPosition);
       return acc + distance;
     }, 0);
   });
@@ -22,6 +31,14 @@ function findShortestAmountOfFuel(inputs) {
   const shortestFuel = fuelCounts.sort((a, b) => a - b)[0];
 
   return shortestFuel;
+}
+
+function findShortestAmountOfFuelWithAcceleration(inputs) {
+  return findShortestAmountOfFuel(inputs, triangleNumbersFn);
+}
+
+function findShortestAmountOfFuelLinear(inputs) {
+  return findShortestAmountOfFuel(inputs, linearFn);
 }
 
 function a() {
@@ -36,5 +53,6 @@ module.exports = {
   a,
   b,
   getPuzzleInput,
-  findShortestAmountOfFuel,
+  findShortestAmountOfFuelLinear,
+  findShortestAmountOfFuelWithAcceleration,
 };
--- src/day07/main.spec.js
+++ src/day07/main.spec.js
@@ -1,15 +1,24 @@
 const { expect } = require("chai");
-const { getPuzzleInput, findShortestAmountOfFuel } = require("./main");
+const {
+  getPuzzleInput,
+  findShortestAmountOfFuelLinear,
+  findShortestAmountOfFuelWithAcceleration,
+} = require("./main");
 
 const testInput = getPuzzleInput(true);
 
 describe("day 07", function () {
   describe("part a", function () {
     it("should find the shortest amount of fuel needed", function () {
-      const fuel = findShortestAmountOfFuel(testInput);
+      const fuel = findShortestAmountOfFuelLinear(testInput);
       expect(fuel).to.equal(37);
     });
   });
 
-  describe("part b", function () {});
+  describe("part b", function () {
+    it("should find the shortest amount of fuel needed", function () {
+      const fuel = findShortestAmountOfFuelWithAcceleration(testInput);
+      expect(fuel).to.equal(168);
+    });
+  });
 });
