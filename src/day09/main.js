300d3de1a52450a41dca3cc7c23ad9738dc98574
Daniel Jackson <d95jackson@gmail.com>
2021-12-19 21:07:23 +0000

    well damn this took longer than expected to artciulate in code...

--- src/day09/main.js
+++ src/day09/main.js
@@ -21,11 +21,18 @@ const getRiskLevel = (lowPoints) => {
   return lowPoints.reduce((acc, v) => acc + v + 1, 0);
 };
 
-function findLowPoints(input) {
-  const width = input[0].length;
-  const height = input.length;
+function getAdjacentHeights(x, y, map) {
+  const values = [
+    ...getHeightAtPosition(x, y - 1, map),
+    ...getHeightAtPosition(x + 1, y, map),
+    ...getHeightAtPosition(x, y + 1, map),
+    ...getHeightAtPosition(x - 1, y, map),
+  ];
+  return values;
+}
 
-  const map = input.reduce((acc, row, y) => {
+function generatePointMap(input) {
+  return input.reduce((acc, row, y) => {
     const split = row.split("");
 
     split.forEach((value, x) => {
@@ -35,6 +42,13 @@ function findLowPoints(input) {
 
     return acc;
   }, {});
+}
+
+function findLowPoints(input) {
+  const width = input[0].length;
+  const height = input.length;
+
+  const map = generatePointMap(input);
 
   const lowPoints = [];
 
@@ -43,20 +57,75 @@ function findLowPoints(input) {
       const key = `${x}-${y}`;
       const value = map[key];
 
-      const values = [
-        ...getHeightAtPosition(x, y - 1, map),
-        ...getHeightAtPosition(x + 1, y, map),
-        ...getHeightAtPosition(x, y + 1, map),
-        ...getHeightAtPosition(x - 1, y, map),
-      ];
+      const values = getAdjacentHeights(x, y, map);
 
       if (values.every((adjacentHeight) => adjacentHeight > value)) {
-        lowPoints.push(value);
+        lowPoints.push({ x, y, value });
       }
     }
   }
 
   return lowPoints;
+}
+
+function isPointValid(x, y, width, height) {
+  return x >= 0 && x < width && y >= 0 && y < height;
+}
+
+function getAdjacentPoints(x, y, width, height) {
+  const points = [];
+
+  if (isPointValid(x, y + 1, width, height)) points.push({ x, y: y + 1 });
+  if (isPointValid(x, y - 1, width, height)) points.push({ x, y: y - 1 });
+  if (isPointValid(x + 1, y, width, height)) points.push({ x: x + 1, y });
+  if (isPointValid(x - 1, y, width, height)) points.push({ x: x - 1, y });
+
+  return points;
+}
+
+// for every point we simulate a flood fill until we find the low point
+function findBasins(input) {
+  // everytime we find the low point we cache where it started and ended
+  const pointCache = {};
+
+  const width = input[0].length;
+  const height = input.length;
+
+  const lowPoints = findLowPoints(input);
+
+  const map = generatePointMap(input);
+
+  const basins = lowPoints.reduce((acc, { x, y }, i) => {
+    const adjacentPoints = getAdjacentPoints(x, y, width, height);
+    const points = [];
+    while (adjacentPoints.length > 0) {
+      const adjacentPoint = adjacentPoints.pop();
+      const adjacentKey = `${adjacentPoint.x}-${adjacentPoint.y}`;
+      const adjacentValue = map[adjacentKey];
+
+      if (pointCache[adjacentKey]) continue;
+      if (adjacentValue === 9) continue;
+
+      points.push({ x: adjacentPoint.x, y: adjacentPoint.y });
+      pointCache[adjacentKey] = true;
+
+      const nextAdjacent = getAdjacentPoints(
+        adjacentPoint.x,
+        adjacentPoint.y,
+        width,
+        height
+      );
+
+      adjacentPoints.push(...nextAdjacent);
+    }
+
+    acc.push(points);
+    return acc;
+  }, []);
+
+  basins.sort((a, b) => b.length - a.length);
+
+  return basins;
 }
 
 function a() {
@@ -65,7 +134,13 @@ function a() {
   console.log("a", riskLevel);
 }
 function b() {
-  console.log("b", input);
+  const basins = findBasins(input);
+  const biggestThreeBasins = basins.slice(0, 3);
+  const basinsArea =
+    biggestThreeBasins[0].length *
+    biggestThreeBasins[1].length *
+    biggestThreeBasins[2].length;
+  console.log("b", basinsArea);
 }
 
 module.exports = {
@@ -74,4 +149,5 @@ module.exports = {
   getPuzzleInput,
   findLowPoints,
   getRiskLevel,
+  findBasins,
 };
--- src/day09/main.spec.js
+++ src/day09/main.spec.js
@@ -1,6 +1,11 @@
 const { expect } = require("chai");
 const { getCurrentDay } = require("../../utils/files");
-const { getPuzzleInput, findLowPoints, getRiskLevel } = require("./main");
+const {
+  getPuzzleInput,
+  findLowPoints,
+  getRiskLevel,
+  findBasins,
+} = require("./main");
 
 const day = getCurrentDay(__dirname);
 const testInput = getPuzzleInput(true);
@@ -9,12 +14,24 @@ describe(`day ${day}`, function () {
   describe("part a", function () {
     it("should find the low points", function () {
       const lowPoints = findLowPoints(testInput);
-      expect(lowPoints).to.deep.equal([1, 5, 5, 0]);
+      const lowPointValues = lowPoints.map(({ value }) => value);
+      expect(lowPointValues).to.deep.equal([1, 5, 5, 0]);
 
-      const riskLevel = getRiskLevel(lowPoints);
+      const riskLevel = getRiskLevel(lowPointValues);
       expect(riskLevel).to.equal(15);
     });
   });
 
-  describe("part b", function () {});
+  describe("part b", function () {
+    it("should flood fill the map into an array of basins", function () {
+      const basins = findBasins(testInput);
+
+      const biggestThreeBasins = basins.slice(0, 3);
+      const basinsArea =
+        biggestThreeBasins[0].length *
+        biggestThreeBasins[1].length *
+        biggestThreeBasins[2].length;
+      expect(basinsArea).to.equal(1134);
+    });
+  });
 });
